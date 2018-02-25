ca5d7c9947fe7fe6cb54c0c1af362886036301ab
wafa-b <wafa_609@hotmail.com>
2018-02-25 20:33:04 +0300

    Add ATM class

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -1,35 +1,49 @@
-def withdraw(balance,request):
-    print("Current balance = " + str(balance))
-    if request > balance:
-        print("Can`t give you all this money !!")
-        print(" ")
-    elif request <= 0:
-        print("More than zero plz!")
-        print(" ")
-    else:
-        balance = balance - request
-        while request > 0:
-            if request >= 100:
-                request -= 100
-                print("give 100")
-            elif request >= 50:
-                request -= 50
-                print ("give 50")
-            elif request >= 10:
-                request -= 10
-                print ("give 10")
-            elif request >= 5:
-                request -= 5
-                print ("give 5")
-            else:
-                print ("give " + str(request))
-                request = 0
-        print(" ")
+class ATM:
+    def __init__(self,balance,bank_name):
+        self.balance = balance
+        self.bank_name = bank_name
 
-    return balance
-balance = 500
-balance=withdraw(balance,277)
-balance=withdraw(balance,30)
-balance=withdraw(balance,5)
-balance=withdraw(balance,700)
-balance=withdraw(balance,-10)
+
+    def withdraw(self,request):
+        print ("Welcome to " + self.bank_name)
+        print("Current balance = " + str(self.balance))
+        print("==================================")
+        if request > self.balance:
+            print("Can`t give you all this money !!")
+            print("==================================")
+        elif request <= 0:
+            print("More than zero plz!")
+            print("==================================")
+        else:
+            self.balance = self.balance - request
+            while request > 0:
+                if request >= 100:
+                    request -= 100
+                    print("give 100")
+                elif request >= 50:
+                    request -= 50
+                    print ("give 50")
+                elif request >= 10:
+                    request -= 10
+                    print ("give 10")
+                elif request >= 5:
+                    request -= 5
+                    print ("give 5")
+                else:
+                    print ("give " + str(request))
+                    request = 0
+            print("==================================")
+        return self.balance
+
+
+balance1 = 500
+balance2 = 1000
+
+atm1 = ATM(balance1, "Smart Bank")
+atm2 = ATM(balance2, "Baraka Bank")
+
+atm1.withdraw(277)
+atm1.withdraw(800)
+
+atm2.withdraw(100)
+atm2.withdraw(2000)
