b1267fb8fe8a55be33e4bbed977ba10247a7b9fd
wafa-b <wafa_609@hotmail.com>
2018-02-27 21:02:05 +0300

    add str fun to print object

--- Forums/models.py
+++ Forums/models.py
@@ -4,7 +4,14 @@ class Member:
         self.age = age
 
 
+    def __str__(self):
+        return  "Name: " + self.name + "\n" +"Age: " + str(self.age)
+
 class Post:
     def __init__(self,title,contente):
         self.title = title
         self.contente = contente
+
+
+    def __str__(self):
+        return "Title: " +self.title + "\n"+ "Contente: "+ self.contente
