ca42878ff281b04bf16874a7ed43fb958f808a32
wafa-b <wafa_609@hotmail.com>
2018-02-27 16:57:55 +0300

    Add main file

---
+++ Forums/main.py
@@ -0,0 +1,13 @@
+import models
+
+
+member1 = models.Member("Ahmed",30)
+member2 = models.Member("Abdullah",20)
+
+
+post1 = models.Post("First Post","This is first post")
+post2 = models.Post("Second Post","This is second post")
+post3 = models.Post("Third Post","This is third post")
+
+
+
