37114436da557c81c87f63b297dd9c9e3c42854b
wafa-b <wafa_609@hotmail.com>
2018-02-27 16:57:43 +0300

    Add models file

---
+++ Forums/models.py
@@ -0,0 +1,10 @@
+class Member:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+
+class Post:
+    def __init__(self,title,contente):
+        self.title = title
+        self.contente = contente
