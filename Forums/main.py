351bb3d09f17545463723b97f8b7fbe9ff6b4344
wafa-b <wafa_609@hotmail.com>
2018-02-28 20:00:21 +0300

    update file

--- Forums/main.py
+++ Forums/main.py
@@ -1,4 +1,5 @@
 import models
+import stores
 
 
 member1 = models.Member("Ahmed",30)
@@ -8,6 +9,13 @@ member2 = models.Member("Abdullah",20)
 post1 = models.Post("First Post","This is first post")
 post2 = models.Post("Second Post","This is second post")
 post3 = models.Post("Third Post","This is third post")
+
+
+member_store = stores.MemberStore()
+member_store.add(member1)
+member_store.add(member2)
+
+print(member_store.get_all())
 
 print member1
 print member2
