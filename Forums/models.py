00c7b7b49206a5e8a5eb480c5c7e5857db73af51
wafa-b <wafa_609@hotmail.com>
2018-03-04 19:26:08 +0300

    Add date

--- Forums/models.py
+++ Forums/models.py
@@ -1,3 +1,4 @@
+import datetime
 class Member:
     def __init__(self,name,age):
         self.name = name
@@ -15,7 +16,7 @@ class Post:
         self.contente = contente
         self.id = 0
         self.member_id = member_id
-
+        self.date = datetime.datetime.now()
 
     def __str__(self):
-        return "Title: " +self.title + "\n"+ "Contente: "+ self.contente
+        return "Title: " +self.title + "\n"+ "Contente: "+ self.contente + "\n" + str(self.date)
